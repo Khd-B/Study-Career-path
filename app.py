ca6fc48429f87eb761c6755e1d925d84dcd8436c
KB <1972mmkb@gmail.com>
2024-11-10 17:11:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sentencepiece
 from transformers import T5Tokenizer, T5ForConditionalGeneration  # Corrected imports for ByT5
 
 # Load the T5 model and tokenizer using st.cache_resource
